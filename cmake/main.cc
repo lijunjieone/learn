85fc58f40803d025b9a10406c759e5fa6731704c
lijunjie <lijunjieone@gmail.com>
2018-08-07 10:21:50 +0800

    增加第二个例子

--- cmake/main.cc
+++ cmake/main.cc
@@ -1,22 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double power(double base,int exponent) {
+#include "MathFunctions.h"
 
-    int result = base;
-    int i;
-
-    if (exponent == 0 ) {
-        return 1;
-    }
-
-    for (i = 1;i<exponent;++i) {
-        result = result * base;
-    }
-
-    return result;
-
-}
 
 
 int main(int argc,char *argv[]) {
