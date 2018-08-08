0ca694012771848cfaf8753f32c8c797791e4fa2
lijunjie <lijunjieone@gmail.com>
2018-08-08 12:40:20 +0800

    learn std::cin

--- essential/test_io.cc
+++ essential/test_io.cc
@@ -5,4 +5,8 @@ void test_cout() {
     std::string val = "this is string test";
     std::cout << "this is a test" <<std::endl;
     std::cout << val <<std::endl;
+    std::cout << "please input some word:"<< std::endl;
+    std::cin>>val;
+    std::cout << "this is your input " << val << std::endl;
+
 }
