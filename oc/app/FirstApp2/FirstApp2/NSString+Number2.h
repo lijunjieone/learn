074831f39b9741e0f5761a64e0193633783f3f4b
lijunjie <lijunjieone@gmail.com>
2018-08-27 10:15:06 +0800

    add category test

---
+++ oc/app/FirstApp2/FirstApp2/NSString+Number2.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Number2.h
+//  FirstApp2
+//
+//  Created by lijunjie on 27/08/2018.
+//  Copyright © 2018 lijunjie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Number2)
+
+@end
---
+++ oc/app/FirstApp2/FirstApp2/NSString+Number2.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Number2.m
+//  FirstApp2
+//
+//  Created by lijunjie on 27/08/2018.
+//  Copyright © 2018 lijunjie. All rights reserved.
+//
+
+#import "NSString+Number2.h"
+
+@implementation NSString (Number2)
+
+@end
