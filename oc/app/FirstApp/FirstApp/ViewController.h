f20358486925c4283a4df893f13a14a7fb4d1ec7
lijunjie <lijunjieone@gmail.com>
2018-09-07 20:32:39 +0800

    集成flutter到ios已有程序中

--- oc/app/FirstApp/FirstApp/ViewController.h
+++ oc/app/FirstApp/FirstApp/ViewController.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Flutter/Flutter.h"
+
 
 @interface ViewController : UIViewController
 
--- oc/app/FirstApp/FirstApp/ViewController.m
+++ oc/app/FirstApp/FirstApp/ViewController.m
@@ -16,9 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button addTarget:self
+               action:@selector(handleButtonAction)
+     forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:@"Press me" forState:UIControlStateNormal];
+    [button setBackgroundColor:[UIColor blueColor]];
+    button.frame = CGRectMake(80.0, 210.0, 160.0, 40.0);
+    [self.view addSubview:button];
 }
 
+- (void)handleButtonAction {
+    FlutterViewController* flutterViewController = [[FlutterViewController alloc] init];
+    [self presentViewController:flutterViewController animated:false completion:nil];
+    [flutterViewController setInitialRoute:@"route1"];
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
